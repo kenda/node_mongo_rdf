e87f27d29b165b8141baea9a4ee74cfc654f0e38
Marcus Nitzschke <marcus.nitzschke@gmx.com>
2011-04-11 23:11:30 +0200

    [Mod] completed insert() and replace(); [Add] getModel()

--- flat.js
+++ flat.js
@@ -16,12 +16,16 @@ var rdf_trip_default1 = {
   }
 };
 var rdf_trip_default2 = {
-  "http://example.org/about" : {
-    "http://purl.org/dc/elements/1.1/superman" : [ { "value" : "Kim Wilder", "type" : "literal" } ],
-    "http://purl.org/dc/elements/1.1/superwoman" : [ { "value" : "Kim Wilder", "type" : "literal" } ]
+  "http://example.org/about2" : {
+    "http://purl.org/dc/elements/1.1/creator" : [ { "value" : "Tom Wilder", "type" : "literal" } ],
+    "http://purl.org/dc/elements/1.1/superwoman" : [ { "value" : "Kim Wilder", "type" : "literal" } ],
+    "http://purl.org/dc/elements/1.1/superman" : [ { "value" : "Gerad Wilder", "lang" : "de" } ]
   }
 };
 
+/**
+ * @class
+ */
 var NodeMongoRdfFlat = function(db, host, port){
   // TODO setting indices
   //   - subject
@@ -41,7 +45,8 @@ var NodeMongoRdfFlat = function(db, host, port){
   var RDFObject = new Schema({
     value: String,
     type: String,
-    lang: { type: String, required: false }
+    lang: { type: String, required: false },
+    value2: { type: String, required: false }
   });
 
   // Connecting to the database
@@ -53,7 +58,11 @@ var NodeMongoRdfFlat = function(db, host, port){
   mongoose.model('RDFTriple', RDFTriple );
   var RDFTripleModel = mongoose.model('RDFTriple');
 
-  // Function for converting RDF/json to Flat Objects
+  /**
+   * Function for converting RDF/json to Flat Objects.
+   * @param {Object} rdf The rdf/json object which will be converted.
+   * @returns {Object} The converted rdf/json object in flat structure.
+   */
   function _rdf_json_to_flat(rdf){
     for (subject in rdf){
       flat = {};
@@ -70,7 +79,11 @@ var NodeMongoRdfFlat = function(db, host, port){
     return flat;
   }
 
-  // Function for converting Flat Objects to RDF/json
+  /**
+   * Function for converting Flat Objects to RDF/json.
+   * @param {Object} flat The flat object which will be converted.
+   * @returns {Object} The converted flat object in rdf/json structure.
+   */
   function _flat_to_rdf_json(flat){
     rdf = {};
     rdf[flat.subject] = {};
@@ -86,17 +99,33 @@ var NodeMongoRdfFlat = function(db, host, port){
   }
 
   return {
-    // Method for saving documents to the database
-    // If a document is already stored, this method
-    // merges the existing with the new document.
+    /**
+     * Method for saving documents to the database.
+     *
+     * If a document is already stored, this method
+     * merges the existing with the new document.
+     * If a new predicate is found it will be simply pushed
+     * with all the objects.
+     * If a existing predicate is found the function checks each
+     * object of the predicate whether the key 'value' is already·
+     * stored. If not so the object will be pushed to the predicate.
+     * Otherwise the object is already stored and non existing keys·
+     * will be pushed to this object.
+     * @param {Object} rdf_triple The triple to insert in rdf/json
+     * @param {function} callback function(error)
+     */
     insert: function(rdf_triple, callback){
+              // calling save() of an object inside an find callback
+              // doesn't seem to work, so we have to use two identical
+              // objects here
               obj = new RDFTripleModel();
+              obj2 = new RDFTripleModel();
 
               obj.doc = _rdf_json_to_flat(rdf_triple);
               obj.save(function(err){
                 if (err){
+                  // Document already stored
                   if (err.message.indexOf('E11000 ') !== -1) {
-                    // Document already stored
 
                     // get existing document
                     RDFTripleModel.findOne({subject: obj.doc.subject}, function(err, doc){
@@ -107,25 +136,59 @@ var NodeMongoRdfFlat = function(db, host, port){
                       // collect all existing predicate uris
                       for (pred in doc.doc.predicates){
                         if (uri = doc.doc.predicates[pred].uri !== undefined)
-                          exist_pred_uris[doc.doc.predicates[pred].uri]=1;
+                          exist_pred_uris[doc.doc.predicates[pred].uri]=pred;
                       }
-
                       // compare predicate uris of the new triple with the
                       // existing ones
                       for (pred in obj.doc.predicates){
                         // if a new predicate is found - push it 
-                        // FIXME pusht unter Umständen doppelt
                         if ( (obj.doc.predicates[pred].uri in exist_pred_uris) == false ){
-                          console.log("neues Prädikat");
                           doc.doc.predicates.push(obj.doc.predicates[pred]);
                         }
                         else{
-                          // TODO else compare the objects
-                          console.log("altes Prädikat");
+                          // predicate already stored - check for different
+                          // objects
+                          for (new_object in obj.doc.predicates[pred].objects){
+                            for (exist_object in doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects){
+                              // if the value key differs mark the object for
+                              // pushing
+                              if (doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects[exist_object].value !== obj.doc.predicates[pred].objects[new_object].value){
+                                push=true;
+                              }
+                              else{
+                                // object with same value key already stored -
+                                // check for new attributes
+                                for (key in obj.doc.predicates[pred].objects[new_object]){
+                                  // console.log("comparing:"+key);
+                                  // console.log(obj.doc.predicates[pred].objects[object]);
+                                  // console.log(doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects[exist_object]);
+                                  // console.log("#############");
+
+                                  if (doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects[exist_object][key] === undefined){
+                                    // key don't exist - set it
+                                    doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects[exist_object][key] = obj.doc.predicates[pred].objects[object][key]
+                                  }
+                                  // because we found an object with the same
+                                  // value key we don't have to push it anymore
+                                  push=false;
+                                }
+                              }
+                            }
+                            // push new object to document
+                            if (push==true)
+                              doc.doc.predicates[exist_pred_uris[obj.doc.predicates[pred].uri]].objects.push(obj.doc.predicates[pred].objects[new_object]);
+                          }
                         }
                       }
-                      doc.save(function(err){
+                      // console.log(doc.doc.predicates[0].objects);
+                      // HACK calling doc.save() doesn't work so we replace the
+                      // existing document
+                      obj2.doc = doc.doc;
+                      doc.remove(function(err){
                           if (err) callback(err);
+                          obj2.save(function(err){
+                            if (err) callback(err)
+                          });
                       });
                     });
                   }
@@ -134,41 +197,62 @@ var NodeMongoRdfFlat = function(db, host, port){
               });
             },
 
-    // Method for replacing existing documents in
-    // the database.
-    // Inserting if the new subject uri isn't stored yet,
-    // else replacing the existing document with the new one.
+    /**
+     * Method for replacing existing documents in the database.
+     *
+     * Inserts if the new subject uri isn't stored yet,
+     * else replaces the existing document with the new one.
+     * @param {Object} rdf_triple The triple to insert in rdf/json
+     * @param {function} callback function(error)
+     */
     replace: function(rdf_triple, callback){
+               // Because saving the same object two times in the
+               // same scope doesn't work, we have to use two 
+               // identical objects here.
                obj = new RDFTripleModel();
+               obj2 = new RDFTripleModel();
 
                obj.doc = _rdf_json_to_flat(rdf_triple);
+               obj2.doc = _rdf_json_to_flat(rdf_triple);
                obj.save(function(err){
                  if (err){
-                   callback(err);
                    if (err.message.indexOf('E11000 ') !== -1) {
                      // Document already stored
                      RDFTripleModel.findOne({subject: obj.doc.subject}, function(err, doc){
-                       if (err) console.log(err);
+                       if (err) callback(err);
                        doc.remove(function(err){
                          if (err) callback(err);
-                         // FIXME save() wird nicht ausgeführt
-                         obj.save(function(err){
+                         obj2.save(function(err){
                            if (err) callback(err);
                          });
                        });
                      });
                    }
+                   else callback(err);
                  }
                });
              },
 
-    // Method for querying one specific document
-    findOne: function(query, callback){
+    /**
+     * Method for querying one specific document.
+     */
+    findOne: function(callback){
                RDFTripleModel.findOne( function(err, doc){
                  if (err) callback(err);
-                 if (doc) callback(null, _flat_to_rdf_json(doc));
+                 else callback(null, _flat_to_rdf_json(doc));
                });
-             }
+             },
+
+    /**
+     * Method which returns the native Mongoose Model instance.
+     *
+     * Therefore you can use all native methods that are not
+     * implemented by node_mongo_rdf.
+     * @returns {Object} mongoose.Model instance
+     */
+    getModel: function(){
+                return RDFTripleModel;
+              }
   }
 }
 
@@ -176,17 +260,22 @@ var NodeMongoRdfFlat = function(db, host, port){
 
 test = NodeMongoRdfFlat("test");
 
-//test.insert(rdf_trip_default1, function(err){
-//  if (err) console.log(err.message);
-//});
-test.replace(rdf_trip_default2, function(err){
-  if (err) console.log(err.message);
-});
-
-//test.findOne({}, function(err, doc){
+// test.insert(rdf_trip_default1, function(err){
+//   if (err) console.log(err.message);
+// });
+// test.insert(rdf_trip_default2, function(err){
+//   if (err) console.log(err.message);
+// });
+// 
+// test.findOne({}, function(err, doc){
 //  if (err) console.log(err);
 //  else console.log(doc);
-//})
+// })
+
+// test.getModel().count({},function(err, count){
+//   if (err) console.log(err);
+//   else console.log(count);
+// });
 
 //mongoose.disconnect();
 
